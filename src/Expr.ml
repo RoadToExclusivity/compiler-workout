f87e6e9423d8da18d69b08598e372fb9772e8004
Dmitry Vinokurov <dimaoq@gmail.com>
2018-02-16 19:45:16 +0300

    -_-

--- src/Expr.ml
+++ src/Expr.ml
@@ -35,13 +35,13 @@ let update x v s = fun y -> if x = y then v else s y
 (* An example of a non-trivial state: *)                                                   
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
-(* Some testing; comment this definition out when submitting the solution. *)
+(* Some testing; comment this definition out when submitting the solution. 
 let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
-    ) ["x"; "a"; "y"; "z"; "t"; "b"]
+    ) ["x"; "a"; "y"; "z"; "t"; "b"] *)
 
 (* Expression evaluator
 
@@ -50,5 +50,29 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+let calc binop x y = 
+	let val_to_bool x = if x = 0 then false else true
+	and logic_calc logic_op x y = if logic_op x y then 1 else 0 in
+	match binop with
+	  "+" -> x + y
+	| "-" -> x - y
+	| "*" -> x * y
+	| "/" -> x / y
+	| "%" -> x mod y
+	| "<" -> logic_calc (<) x y
+	| ">" -> logic_calc (>) x y
+	| "<=" -> logic_calc (<=) x y
+	| ">=" -> logic_calc (>=) x y
+	| "==" -> logic_calc (=) x y
+	| "!=" -> logic_calc (<>) x y
+	| "&&" -> logic_calc (&&) (val_to_bool x) (val_to_bool y)
+	| "!!" -> logic_calc (||) (val_to_bool x) (val_to_bool y);;
+	
+let rec eval s expression =
+	match expression with
+	  Const (value) -> value
+	| Var (x) -> s x
+	| Binop (binop, x, y) -> 
+		let res_x = eval s x
+		and res_y = eval s y in
+		calc binop res_x res_y;;
