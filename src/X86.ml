713b66c3d100124aa61a78058b6a8ceaa09f8b05
Dmitry Vinokurov <dimaoq@gmail.com>
2018-04-22 00:17:34 +0300

    HW8

--- src/X86.ml
+++ src/X86.ml
@@ -90,7 +90,84 @@ open SM
    Take an environment, a stack machine program, and returns a pair --- the updated environment and the list
    of x86 instructions
 *)
-let compile env code = failwith "Not implemented"
+let rec compile env code =
+	let make_mov x y =
+		(match x, y with 
+		| M _, S _ | S _, S _ | S _, M _ -> [Mov (x, edx); Mov (edx, y)]
+		| _, _ -> [Mov (x, y)]) in
+	let rec create_binop op x y = 
+		let logic_op op x y = [Mov (y, edx); Binop (op, x, edx)] in
+		(match op with
+		| "+" -> [Mov (x, eax); Binop ("+", y, eax)], eax
+		| "-" -> [Mov (x, eax); Binop ("-", y, eax)], eax
+		| "*" -> [Mov (x, eax); Binop ("*", y, eax)], eax
+		| "/" -> [Mov (x, eax); Cltd; IDiv y], eax
+		| "%" -> [Mov (x, eax); Cltd; IDiv y], edx
+		| ">" -> (logic_op "cmp" y x) @ [Mov (L 0, eax); Set ("g", "%al")], eax
+		| "<" -> (logic_op "cmp" x y) @ [Mov (L 0, eax); Set ("g", "%al")], eax
+		| ">=" -> (logic_op "cmp" y x) @ [Mov (L 0, eax); Set ("ge", "%al")], eax
+		| "<=" -> (logic_op "cmp" x y) @ [Mov (L 0, eax); Set ("ge", "%al")], eax
+		| "==" -> (logic_op "cmp" x y) @ [Mov (L 0, eax); Set ("e", "%al")], eax
+		| "!=" -> (logic_op "cmp" x y) @ [Mov (L 0, eax); Set ("ne", "%al")], eax
+		| "&&" -> 
+			let x_conv, reg_x = create_binop "!=" x (L 0)
+			and y_conv, reg_y = create_binop "!=" y (L 0) in
+			x_conv @ [Mov (reg_x, x)] @ y_conv @ [Mov (reg_y, y)] @ (logic_op "&&" x y) @ [Mov (L 0, eax); Set ("nz", "%al")], eax
+		| "!!" -> (logic_op "!!" x y) @ [Mov (L 0, eax); Set ("nz", "%al")], eax
+		| _ -> failwith "Not implemented binary operator %s\n" @@ op) in
+	match code with
+	| [] -> (env, [])
+	| insn::rest ->
+		let env', ops = 
+			(match insn with
+			| BINOP op -> 
+				let y, x, env = env#pop2 in
+				let e, env = env#allocate
+				and new_ops, acc = create_binop op x y in
+				(env, new_ops @ (make_mov acc e))
+			| CONST n ->
+				let x, env = env#allocate in
+				(env, make_mov (L n) x)
+			| READ ->
+				let e, env = env#allocate in
+				(env, [Call "Lread"] @ make_mov eax e)
+			| WRITE ->
+				let e, env = env#pop in
+				(env, [Push e; Call "Lwrite"; Pop edx])
+			| LD x ->
+				let e, env = env#allocate in
+				(env, make_mov (env#loc x) e)
+			| ST x -> 
+				let e, env = (env#global x)#pop in
+				(env, make_mov e (env#loc x))
+			| LABEL l -> (env, [Label l])
+			| JMP l -> (env, [Jmp l])
+			| CJMP (op, l) -> 
+				let e, env = env#pop in
+				(env, [Binop ("cmp", L 0, e); CJmp (op, l)])
+			| BEGIN (name, args, locals) -> 
+				let env' = env#enter name args locals in
+				env', [Push ebp; Mov (esp, ebp); Binop ("-", M ("$" ^ (env'#lsize)), esp)]
+			| END -> env, [Label (env#epilogue); Mov (ebp, esp); Pop ebp; Ret; Meta (Printf.sprintf "\t.set\t%s,\t%d" env#lsize (env#allocated * word_size))]
+			| CALL (name, argnum, isFunc) -> 
+				let func_map y lst = List.map y lst in
+				let push_reg = func_map (fun x -> Push x) env#live_registers in
+				let pop_reg = List.rev (func_map (fun x -> Pop x) env#live_registers) in
+				let rec compile_args env' = function
+					| 0 -> env', []
+					| n -> (let res, env'' = env'#pop in 
+							let env'', st = compile_args env'' (n - 1) in 
+							env'', st @ [Push res]) in
+				let env', args = compile_args env argnum in
+				let env', func_add = 
+				   (if isFunc then let e, env'' = env'#allocate in env'', make_mov eax e 
+					else env', []) in
+				env', push_reg @ args @ [Call name; Binop ("+", L (word_size * argnum), esp)] @ pop_reg @ func_add
+			| RET isFunc ->
+				if isFunc then let ret, env' = env#pop in env', (make_mov ret eax) @ [Jmp env'#epilogue]
+				else env, [Jmp env#epilogue]) in
+		let newenv, op_list = compile env' rest in
+		(newenv, ops @ op_list);;
                                 
 (* A set of strings *)           
 module S = Set.Make (String)
@@ -118,9 +195,9 @@ class env =
       let x, n =
 	let rec allocate' = function
 	| []                            -> ebx     , 0
-	| (S n)::_                      -> S (n+1) , n+1
+	| (S n)::_                      -> S (n+1) , n+2
 	| (R n)::_ when n < num_of_regs -> R (n+1) , stack_slots
-        | (M _)::s                      -> allocate' s
+    | (M _)::s                      -> allocate' s
 	| _                             -> S 0     , 1
 	in
 	allocate' stack
